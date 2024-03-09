bbc761127a49108bc5350fc676c69863bb795b61
unknown <jaco20obie@gmail.com>
2024-03-09 11:14:18 +0200

    my firts commit

---
+++ hangman.py
@@ -0,0 +1,31 @@
+def hangman():
+    word="cat"
+    wrong_guesses=0
+    stages=['','___ ','| | ','| 0','|/|\\ ','|/ \\ ','|    ']
+    letters_left=list(word)
+    score_board=['___']*len(word)
+    win=False
+    print('Welcome to hang man')
+
+    while wrong_guesses <len(stages)-1:
+        print('\n')
+        guess=input("guess a letter")
+        if guess in letters_left:
+            character_index=letters_left.index(guess)
+            score_board[character_index]=guess
+            letters_left[character_index]='$'
+        else:
+            wrong_guesses +=1
+            print(''.join(score_board))
+            print('\n'.join(stages[0:wrong_guesses+1]))
+        if '___' not in score_board:
+            print('you win! The word was {}'.format(word))
+            win=True
+            break
+
+    if not win:
+        print('\n'.join(stages[0:wrong_guesses]))
+        print('You lose! The word was {}'.format(word))
+            
+
+hangman()
